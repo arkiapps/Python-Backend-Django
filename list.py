95e4d1ee016a10077182f918a7a767ae5ea5d706
melomatthewdev <devscustomers@outlook.com>
2025-02-09 15:06:21 -0500

    list and replacement

---
+++ list.py
@@ -0,0 +1,6 @@
+countries = ['United Kingdom', 'United States', 'Canada', 'Australia', 'Finlandia']
+print(countries)
+print(countries[2])
+print(countries[-1])
+print(type(countries))
+print(len(countries))
