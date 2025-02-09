d1b004bebf30ce57f74086a3c5aa51c64f27b3f7
melomatthewdev <devscustomers@outlook.com>
2025-02-09 14:26:52 -0500

    number and entradas

---
+++ number.py
@@ -0,0 +1,20 @@
+number =79
+
+print(number)
+print(35)
+print(79 + 35)
+print(79 + 22.934)
+print(79 - 22.934)
+print(79 * 22.934)
+print(79 / 22.934)
+print(79 // 22.934)
+print(79 % 22.934)
+print(79 ** 22.934)
+
+number1 = 55
+number2 = str(number1)
+print(number1/6)
+print('number is ' + number2)
+
+from math import *
+print(sqrt(100))
