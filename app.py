190c0630b24cd344433ef7b4979803cf153cd9f8
melomatthewdev <devscustomers@outlook.com>
2025-02-09 14:01:29 -0500

    init course

---
+++ app.py
@@ -0,0 +1,14 @@
+print('Hello World!, Welcome to Django!')
+print('first class python backend')
+print('Hi.\n How are you?')
+print('Hi.\t How are you?')
+
+# variables
+name = 'Melo Matthew'
+age = '35'
+
+print('My name is ' + name)
+print('My age is ' + age)
+print(name+ ' is a boy')
+print(name+ ' is ', age)
+print(name+ ' is from Colombia')
